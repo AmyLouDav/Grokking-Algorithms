aa2d26ab96d4449f151dfdc22b85d2b971981ea8
Amy Davies <amyloudav@gmail.com>
2022-10-06 16:23:17 +0200

    chapter 2 array done

---
+++ chapter-2.js
@@ -0,0 +1,57 @@
+const list = [
+  {
+    A: {
+      value: Amy,
+      next: {
+        value: Aziz,
+        next: {
+          value: Ada,
+          next: {
+            value: Annie,
+            next: {
+              value: Agatha,
+              next: null,
+            },
+          },
+        },
+      },
+    },
+  },
+
+  {
+    B: {
+      value: Ben,
+      next: {
+        value: Betty,
+        next: {
+          value: Bertie,
+          next: {
+            value: Bonnie,
+            next: null,
+          },
+        },
+      },
+    },
+  },
+
+  {
+    C: {
+      value: Clyde,
+      next: {
+        value: Cookie,
+        next: {
+          value: Craig,
+          next: {
+            value: Cody,
+            next: {
+              value: Coco,
+              next: {
+                value: null,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+];
